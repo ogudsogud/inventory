1fc80ecbf21faa9c9beb9bba0ec1dfe73e40dc2b
yoga.wiguna <ogudsogud@gmail.com>
2018-07-16 17:15:56 +0700

    penambahan dan perubahan tabel

--- msg_part/src/main/java/com/msg/data/model/PartOutModel.java
+++ msg_part/src/main/java/com/msg/data/model/PartOutModel.java
@@ -5,7 +5,7 @@ package com.msg.data.model;
  */
 public class PartOutModel {
 
-    private int id_stock_parts;
+    private int id_unit_parts;
     private int id_unit_institution;
     private String ticket_no;
     private String part_name;
@@ -20,12 +20,12 @@ public class PartOutModel {
     private String approved_on;
     private int status;
 
-    public PartOutModel(int id_stock_parts, int id_unit_institution, String ticket_no, String part_name, String part_number,
+    public PartOutModel(int id_unit_parts, int id_unit_institution, String ticket_no, String part_name, String part_number,
                         String institution_name, String unit_name, String description, int quantity,
                         String requested_by, String requested_on,
                         String approved_by, String approved_on, int status) {
 
-        this.id_stock_parts = id_stock_parts;
+        this.id_unit_parts = id_unit_parts;
         this.id_unit_institution = id_unit_institution;
         this.ticket_no = ticket_no;
         this.part_name = part_name;
@@ -45,12 +45,12 @@ public class PartOutModel {
 
     }
 
-    public int getId_stock_parts() {
-        return id_stock_parts;
+    public int getId_unit_parts() {
+        return id_unit_parts;
     }
 
-    public void setId_stock_parts(int id_stock_parts) {
-        this.id_stock_parts = id_stock_parts;
+    public void setId_unit_parts(int id_unit_parts) {
+        this.id_unit_parts = id_unit_parts;
     }
 
     public int getId_unit_institution() {
